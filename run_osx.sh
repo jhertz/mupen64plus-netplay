ff88cc9429471a9759a0d96fe716eac4a6441b2a
jhertz <hectohertz@gmail.com>
2018-01-30 01:06:26 -0500

    added dylib fallback, still not working

--- run_osx.sh
+++ run_osx.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+DYLD_FALLBACK_LIBRARY_PATH="./usr"
 mupen64plus-ui-console/projects/unix/mupen64plus \
 	--corelib ./mupen64plus-core/projects/unix/libmupen64plus.dylib \
 	--netplay ./netplay/build/lib/mupen64plus-netplay.dylib \
